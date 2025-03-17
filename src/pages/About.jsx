bc368ecf4b693d4045d70d9b4383df7de199effc
xXHunterRaiXx <hunter.rai1221@gmail.com>
2025-03-17 15:34:57 -0500

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function About() {
+  return (
+    <>
+      <p>This is the about page</p>;
+    </>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function Home() {
+  return (
+    <>
+      <p>This is the home page</p>;
+    </>
+  );
+}
+
+export default Home;
