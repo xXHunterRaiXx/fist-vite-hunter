f173779476479d9c931b75375139dc0d8bb641aa
xXHunterRaiXx <hunter.rai1221@gmail.com>
2025-03-18 14:19:22 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
